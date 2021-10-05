b582be4da8f88b90001583b1d735060441c45eeb
John Colagioia <john@colagioia.net>
2021-10-05 06:40:20 -0400

    Add basic update notification loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate notify;
+
+use notify::{Watcher, RecursiveMode, watcher};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+  let (tx, rx) = channel();
+  let mut watcher = watcher(tx, Duration::from_secs(10)).unwrap();
+  watcher.watch(".", RecursiveMode::Recursive).unwrap();
+  loop {
+    match rx.recv() {
+      Ok(event) => println!("{:?}", event),
+      Err(e) => println!("watch error: {:?}", e),
+    }
+  }
 }
