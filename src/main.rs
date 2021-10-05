4988e5ade5617a3d236e0032d8458a251e670e9e
John Colagioia <john@colagioia.net>
2021-10-05 06:42:10 -0400

    Choose folders to watch based on configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
+extern crate dirs;
 extern crate notify;
 
+use gjson;
 use notify::{Watcher, RecursiveMode, watcher};
+use std::fs;
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
 fn main() {
+  let mut config_path = dirs::config_dir()
+    .expect("Can't access configuration folder.");
+  config_path.push("intern.json");
+
+  let config_file = fs::read_to_string(config_path)
+    .expect("Unable to read configuration file.");
+  let config = gjson::parse(&config_file);
   let (tx, rx) = channel();
-  let mut watcher = watcher(tx, Duration::from_secs(10)).unwrap();
-  watcher.watch(".", RecursiveMode::Recursive).unwrap();
+  let check_period = config.get("period").u64();
+  let mut watcher = watcher(tx, Duration::from_secs(check_period)).unwrap();
+
+  for folder in config.get("folder").array() {
+    let recurse = if folder.get("recurse").bool() {
+      RecursiveMode::Recursive
+    } else {
+      RecursiveMode::NonRecursive
+    };
+
+    watcher.watch(folder.get("name").str(), recurse).unwrap();
+  }
+
   loop {
     match rx.recv() {
       Ok(event) => println!("{:?}", event),
